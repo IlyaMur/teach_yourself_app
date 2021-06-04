163f4f051fe400bf4173c7163c3b6895d80c02eb
Ilya Mur <ilyamur@hotmail.com>
2021-06-04 22:01:06 +0300

    add devise trackable

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
-  
+  resources :users
+
   root to: 'static_pages#landing_page'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
 end
