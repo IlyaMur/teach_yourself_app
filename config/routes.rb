2f1a8e67a6d105849ee89a50cdab4e7061f89601
Ilya Mur <ilyamur@hotmail.com>
2021-06-04 14:04:04 +0300

    add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :courses
   
-  root to: 'courses#index'
+  root to: 'static_pages#landing_page'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
 end
