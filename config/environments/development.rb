3975a9dc0061536aaf27681ea6d6dd6ba6719c53
Ilya Mur <ilyamur@hotmail.com>
2021-06-08 00:01:59 +0300

    add confirmable to devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.raise_delivery_errors = true
 
   # Uncomment if you wish to allow Action Cable access from any origin.
