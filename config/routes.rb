254b35c26687146f5bee9858a1d33e3352ae24a5
Ilya Mur <ilyamur@hotmail.com>
2021-06-08 16:56:04 +0300

    add show update to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
-  resources :users
+  resources :users, only: %i[index edit show update]
 
   root to: 'static_pages#landing_page'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
